e9680e54b7805e61be542c7168cf135fd8c896b0
Taiwo <taiwokelvin@gmail.com>
2021-07-10 05:17:46 -0700

    portfolio project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,98 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="app-wrapper">
+    <div class="app">
+      <Navigation/>
+      <Home />
+      <router-view />
+    </div>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+export default {
+  name: 'app',
+  components: { Navigation, Home }
+}
+</script>
+
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Ubuntu:wght@400;500;700&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  // display: flex;
+  // flex-direction: column;
+  // min-height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.container {
+  max-width: 1300px;
+  padding: 0 80px;
+  margin: 0 auto;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+section {
+  padding: 100px 0;
+
+  .title {
+    text-align: center;
+    position: relative;
+    font-size: 40px;
+    font-weight: 500;
+    margin-bottom: 60px;
+    padding-bottom: 20px;
+    font-family: 'Ubuntu', sans-serif;
+
+    &::before {
+    content: '';
+    position: absolute;
+    width: 180px;
+    height: 3px;
+    background: #111;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+    &::after {
+    position: absolute;
+    background: #fff;
+    color: crimson;
+    bottom: -12px;
+    left: 50%;
+    padding: 5px;
+    font-size: 20px;
+    transform: translateX(-50%);
+  }
+  }
 }
+
+.font-family {
+  font-family: 'Poppins', sans-serif;
+}
+
+.button {
+  display: inline-block;
+  background: crimson;
+  color: #fff;
+  font-weight: 500;
+  margin-top: 20px;
+  border-radius: 6px;
+  border: 2px solid crimson;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background: transparent;
+    color: crimson;
+  }
+}
+
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,77 @@
+<template>
+  <header>
+      <div class="container">
+            <div class="brand">
+                <router-link to='#'>Portfo<span>lio.</span></router-link>
+            </div>
+            <nav class="nav-links">
+                <router-link to="#" class="link">Home</router-link>
+                <router-link to="#" class="link">About</router-link>
+                <router-link to="#" class="link">Works</router-link>
+                <router-link to="#" class="link">Skill</router-link>
+            </nav>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    width: 100%;
+    padding: 30px 0;
+    position: fixed;
+    z-index: 99;
+    font-family: 'Ubuntu', sans-serif;
+    transition: all 0.3s ease;
+
+    .container {
+        display: flex;
+        align-items: center;
+
+        .brand {
+            flex: 1;
+            a {
+                color: #fff;
+                font-size: 35px;
+                font-weight: 600;
+
+                span {
+                    color: crimson;
+                }
+            }
+        }
+        .nav-links {
+            .link {
+                color: #fff;
+                font-size: 18px;
+                font-weight: 500;
+                margin-right: 25px;
+                transition: color 0.3s ease;
+
+                &.link:last-child {
+                    margin-right: 0;
+                }
+
+                &:hover {
+                    color: crimson;
+                }
+            }
+        }
+        .menu-btn {
+            color: #fff;
+            font-size: 23px;
+            cursor: pointer;
+            display: none;
+        }
+    }
+
+}
+</style>
