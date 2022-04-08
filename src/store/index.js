d66805e8c06a5988214185632d68f270393a22fb
jtk09 <taiwokelvin@gmail.com>
2022-04-08 09:38:33 +0100

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -10,36 +10,46 @@ export default createStore({
       },
       {
         id: 2,
+        image: "eventark.png",
+        link: " https://eventark.com",
+      },
+      {
+        id: 3,
+        image: "driversng.png",
+        link: " https://driversng.com",
+      },
+      {
+        id: 4,
         image: "beachResort.png",
         link: " https://beach-resort007.netlify.app",
       },
       {
-        id: 3,
+        id: 5,
         image: "alan.png",
         link: "https://ai-news-application.netlify.app",
       },
       {
-        id: 4,
+        id: 6,
         image: "portfolio.png",
         link: "https://jtaiwo.netlify.app",
       },
       {
-        id: 5,
+        id: 7,
         image: "makeup.png",
         link: "https://thoughts-glam.netlify.app/",
       },
       {
-        id: 6,
+        id: 8,
         image: "chat.png",
         link: "https://socialapp-53957.web.app",
       },
+      // {
+      //   id: 9,
+      //   image: "ecommerce.png",
+      //   link: "https://clone-435ec.web.app",
+      // },
       {
-        id: 7,
-        image: "ecommerce.png",
-        link: "https://clone-435ec.web.app",
-      },
-      {
-        id: 8,
+        id: 9,
         image: "vca.png",
         link: "https://vca-jtk.netlify.app",
       },
