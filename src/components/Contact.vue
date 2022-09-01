12b5d8ecb0ca8e06b8b3551ff71ab67716f3b124
jtk09 <taiwokelvin@gmail.com>
2022-09-01 20:13:29 +0100

    update portfolio

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,143 +1,183 @@
 <template>
   <section class="contact" id="contact">
-        <div class="container">
-            <h2 class="title">Contact me</h2>
-            <div class="content" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
-                <div class="right">
-                    <form @submit.prevent="sendEmail">
-                        <div class="field">
-                            <input type="text" placeholder="Name" name="name" v-model="name" required>
-                        </div>
-                        <div class="field">
-                            <input type="email" placeholder="Email" name="email" v-model="email" required>
-                        </div>
-                        <div class="field">
-                            <input type="text" placeholder="Subject" name="subject" v-model="subject" required>
-                        </div>
-                        <div class="textarea">
-                            <textarea v-model="message" name="message" required></textarea>
-                        </div>
-                        <div class="btn">
-                            <button type="submit">Send message</button>
-                        </div>
-                    </form>
-                </div>
+    <div class="container">
+      <h2 class="title">Contact me</h2>
+      <div
+        class="content"
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        <div class="right">
+          <form @submit.prevent="sendEmail">
+            <div class="field">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                v-model="name"
+                required
+              />
             </div>
+            <div class="field">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                v-model="email"
+                required
+              />
+            </div>
+            <div class="field">
+              <input
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                v-model="subject"
+                required
+              />
+            </div>
+            <div class="textarea">
+              <textarea v-model="message" name="message" required></textarea>
+            </div>
+            <div class="btn">
+              <button type="submit">
+                {{ loading ? "Sending..." : "Send message" }}
+              </button>
+            </div>
+          </form>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import emailjs from 'emailjs-com';
-import { createToast } from 'mosha-vue-toastify';
-import 'mosha-vue-toastify/dist/style.css'
+import emailjs from "@emailjs/browser";
+import { createToast } from "mosha-vue-toastify";
+import "mosha-vue-toastify/dist/style.css";
 
 export default {
-name: 'contact',
-data(){
+  name: "contact",
+  data() {
     return {
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-        errors: '',
-    }
-},
-methods: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      errors: "",
+      loading: false,
+    };
+  },
+  methods: {
     sendEmail(e) {
-      emailjs.sendForm('service_5mvmk79', 'template_u4cs6j9', e.target, 'user_k4aESwB4eZ5t902ZlAVe8', {
-          message: this.message,
-          name: this.name,
-          subject: this.subject,
-          email: this.email,
-      })
+      this.loading = true;
+      emailjs
+        .sendForm(
+          process.env.VUE_APP_SERVICE,
+          process.env.VUE_APP_TEMPLATE,
+          e.target,
+          process.env.VUE_APP_PK
+        )
         .then(() => {
-            createToast({
-                title: 'Message sent successfully',
-                description: 'Thanks for reaching out.'
-            }, {
-                position: 'top-right',
-                type: 'success',
-                transition: 'bounce',
-                showIcon: 'true',
-                hideProgressBar: 'true',
-                timeout: 5000
-            })
+          this.loading = false;
+          createToast(
+            {
+              title: "Message sent successfully",
+              description: "Thanks for reaching out.",
+            },
+            {
+              position: "top-right",
+              type: "success",
+              transition: "bounce",
+              showIcon: "true",
+              hideProgressBar: "true",
+              timeout: 5000,
+            }
+          );
+          this.message = "";
+          this.name = "";
+          this.subject = "";
+          this.email = "";
         })
-        .catch(err => console.log(err))
-        this.message = '';
-        this.name = '';
-        this.subject = '';
-        this.email = '';
-    }
-}
-}
+        .catch(() => {
+          this.loading = false;
+          createToast(
+            {
+              description: "Oop! An error occur while sending your message",
+            },
+            { type: "danger", showIcon: "true" }
+          );
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .contact {
-    margin-bottom: 40px;
-    .title {
-        &::after {
-            content: 'get in touch';
-        }
+  margin-bottom: 40px;
+  .title {
+    &::after {
+      content: "get in touch";
     }
-    .content {
-        background: lightgray;
-        padding: 30px 15px;
-        
-        .right {
-            max-width: 500px;
-            margin: auto;
-        }
-        .right {
-            form {
-                .textarea {
-                    height: 100px;
-                    width: 100%;
+  }
+  .content {
+    background: lightgray;
+    padding: 30px 15px;
 
-                    textarea {
-                        height: 100%;
-                        width: 100%;
-                        border: 1px solid lightgrey;
-                        border-radius: 6px;
-                        outline: none;
-                        padding: 5px 10px;
-                        font-size: 15px;
-                        font-family: "Poppins", sans-serif;
-                        margin-bottom: 10px;
-                        resize: none;
-                    }
-                }
-                .field {
-                    margin-bottom: 10px;
-                    input {
-                        height: 100%;
-                        width: 100%;
-                        border: 1px solid lightgrey;
-                        border-radius: 6px;
-                        outline: none;
-                        padding: 8px 10px;
-                        font-size: 15px;
-                        font-family: "Poppins", sans-serif;
-                    }
-                }
-
-                button {
-                    font-size: 15px;
-                    font-weight: 500;
-                    background: crimson;
-                    color: #fff;
-                    padding: 10px 16px;
-                    border-radius: 8px;
-                    border: none;
-                    margin-top: 10px;
-                    &:focus {
-                        outline: none;
-                    }
-                }
-            }
-        }
+    .right {
+      max-width: 500px;
+      margin: auto;
     }
+    .right {
+      form {
+        .textarea {
+          height: 100px;
+          width: 100%;
+
+          textarea {
+            height: 100%;
+            width: 100%;
+            border: 1px solid lightgrey;
+            border-radius: 6px;
+            outline: none;
+            padding: 5px 10px;
+            font-size: 15px;
+            font-family: "Poppins", sans-serif;
+            margin-bottom: 10px;
+            resize: none;
+          }
+        }
+        .field {
+          margin-bottom: 10px;
+          input {
+            height: 100%;
+            width: 100%;
+            border: 1px solid lightgrey;
+            border-radius: 6px;
+            outline: none;
+            padding: 8px 10px;
+            font-size: 15px;
+            font-family: "Poppins", sans-serif;
+          }
+        }
+
+        button {
+          font-size: 15px;
+          font-weight: 500;
+          background: crimson;
+          color: #fff;
+          padding: 10px 16px;
+          border-radius: 8px;
+          border: none;
+          margin-top: 10px;
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -12,15 +12,21 @@ export default createStore({
       {
         id: 2,
         image:
-          "https://res.cloudinary.com/citi-tasker/image/upload/v1652804683/eventark_md6wfy.png",
-        link: " https://eventark.com",
+          "https://res.cloudinary.com/citi-tasker/image/upload/v1662047289/jtk-store-nw-1_iqlri5.jpg",
+        link: " https://jtk-sneakers.netlify.app/",
       },
-      {
-        id: 3,
-        image:
-          "https://res.cloudinary.com/citi-tasker/image/upload/v1652805544/driversng_cn6xch.png",
-        link: " https://driversng.com",
-      },
+      // {
+      //   id: 2,
+      //   image:
+      //     "https://res.cloudinary.com/citi-tasker/image/upload/v1652804683/eventark_md6wfy.png",
+      //   link: " https://eventark.com",
+      // },
+      // {
+      //   id: 3,
+      //   image:
+      //     "https://res.cloudinary.com/citi-tasker/image/upload/v1652805544/driversng_cn6xch.png",
+      //   link: " https://driversng.com",
+      // },
       {
         id: 4,
         image:
@@ -33,35 +39,35 @@ export default createStore({
           "https://res.cloudinary.com/citi-tasker/image/upload/v1652804576/alan_pok3up.png",
         link: "https://ai-news-application.netlify.app",
       },
-      {
-        id: 6,
-        image:
-          "https://res.cloudinary.com/citi-tasker/image/upload/v1652804799/portfolio_aw96ou.png",
-        link: "https://jtaiwo.netlify.app",
-      },
+      // {
+      //   id: 6,
+      //   image:
+      //     "https://res.cloudinary.com/citi-tasker/image/upload/v1652804799/portfolio_aw96ou.png",
+      //   link: "https://jtaiwo.netlify.app",
+      // },
       {
         id: 7,
         image:
-          "https://res.cloudinary.com/citi-tasker/image/upload/v1652805688/makeup_nmpb9u.png",
+          "https://res.cloudinary.com/citi-tasker/image/upload/v1662047664/mkup-1_dhgddd.jpg",
         link: "https://thoughts-glam.netlify.app/",
       },
-      {
-        id: 8,
-        image:
-          "https://res.cloudinary.com/citi-tasker/image/upload/v1652804651/chat_on3dye.png",
-        link: "https://socialapp-53957.web.app",
-      },
+      // {
+      //   id: 8,
+      //   image:
+      //     "https://res.cloudinary.com/citi-tasker/image/upload/v1652804651/chat_on3dye.png",
+      //   link: "https://socialapp-53957.web.app",
+      // },
       // {
       //   id: 9,
       //   image: "ecommerce.png",
       //   link: "https://clone-435ec.web.app",
       // },
-      {
-        id: 9,
-        image:
-          "https://res.cloudinary.com/citi-tasker/image/upload/v1652804878/vca_jkcngj.png",
-        link: "https://vca-jtk.netlify.app",
-      },
+      // {
+      //   id: 9,
+      //   image:
+      //     "https://res.cloudinary.com/citi-tasker/image/upload/v1652804878/vca_jkcngj.png",
+      //   link: "https://vca-jtk.netlify.app",
+      // },
     ],
   },
   mutations: {},
